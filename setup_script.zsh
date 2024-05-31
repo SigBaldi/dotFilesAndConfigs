837c8d10cd769caf427e52a0cbb36a0df3f2a245
Daniele Baelde <daniele.baelde@slicelife.com>
2024-05-31 01:14:24 +0100

    Fixed infinite loop

--- setup_script.zsh
+++ setup_script.zsh
@@ -2,14 +2,14 @@
 
 # Install Homebrew if not already installed
 if ! command -v brew &> /dev/null; then
-    echo -e "Homebrew not found. Installing Homebrew..."
+    echo -e "\nHomebrew not found. Installing Homebrew..."
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
     eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
 
 # Update Homebrew and install essential packages
-echo -e "Updating Homebrew and installing essential packages..."
+echo -e "\nUpdating Homebrew and installing essential packages..."
 brew update
 brew install asdf bat dog dust eza fd ffmpegthumbnailer fzf git git-cliff git-delta inlyne jq lazygit poppler ripgrep starship unar wezterm xh yazi zellij zoxide
 
@@ -17,38 +17,39 @@ brew install --cask font-jetbrains-mono-nerd-font insomnia obsidian rectangle we
 
 # Check for existing .fzf.* files before setting up fzf key bindings and fuzzy completion
 if [ ! -f ~/.fzf.zsh ]; then
-    echo "Setting up fzf key bindings and fuzzy completion..."
+    echo "\nSetting up fzf key bindings and fuzzy completion..."
     $(brew --prefix)/opt/fzf/install --zsh
 fi
 
 # Download shell configuration
-echo -e "Downloading shell configuration..."
+echo -e "\nDownloading shell configuration..."
 curl -o ~/.zshrc https://raw.githubusercontent.com/SigBaldi/dotFilesAndConfigs/main/.zshrc
 
 # Download custom aliases
-echo -e "Downloading custom aliases..."
+echo -e "\nDownloading custom aliases..."
 curl -o ~/.zsh_aliases https://raw.githubusercontent.com/SigBaldi/dotFilesAndConfigs/main/.zsh_aliases
 
 # Download tools configuration
-echo -e "Downloading basic Yazi configuration..."
+echo -e "\nDownloading basic Yazi configuration..."
 mkdir -p ~/.config/yazi
 curl -o ~/.config/yazi/keymap.toml https://raw.githubusercontent.com/sxyazi/yazi/latest/yazi-config/preset/keymap.toml
 curl -o ~/.config/yazi/theme.toml https://raw.githubusercontent.com/sxyazi/yazi/latest/yazi-config/preset/theme.toml
 curl -o ~/.config/yazi/yazi.toml https://raw.githubusercontent.com/sxyazi/yazi/latest/yazi-config/preset/yazi.toml
 
 # Source .zshrc to apply changes
-echo -e "Sourcing .zshrc to apply changes..."
+echo -e "\nSourcing .zshrc to apply changes..."
 source ~/.zshrc
 
-echo -e "Shell setup complete!"
+echo -e "\nShell setup complete!"
 
 # Function to display a menu and get the user's choice
 show_menu() {
-    echo -e "Which additional setup scripts do you want to run?"
+    echo "Which additional setup scripts do you want to run?"
     echo "1) Git setup"
     echo "2) asdf setup"
     echo "3) None / Done"
-    read -p "Enter your choice [1-3]: " choice
+    echo -n "Enter your choice [1-3]: "
+    read choice
     return $choice
 }
 
@@ -61,14 +62,14 @@ while true; do
     choice=$?
     case $choice in
         1)
-            echo -e "Downloading Git setup script..."
+            echo -e "\nDownloading Git setup script..."
             xh -o $temp_dir/setup_git.zsh https://raw.githubusercontent.com/SigBaldi/dotFilesAndConfigs/master/setup_git.zsh
             chmod +x $temp_dir/setup_git.zsh
             $temp_dir/setup_git.zsh
             ;;
         2)
-            echo -e "Downloading asdf setup script..."
-            xh -o $temp_dir/setup_asdf.sh https://raw.githubusercontent.com/SigBaldi/dotFilesAndConfigs/master/setup_asdf.sh
+            echo -e "\nDownloading asdf setup script..."
+            xh -o $temp_dir/setup_asdf.zsh https://raw.githubusercontent.com/SigBaldi/dotFilesAndConfigs/master/setup_asdf.zsh
             chmod +x $temp_dir/setup_asdf.sh
             $temp_dir/setup_asdf.sh
             ;;
@@ -86,5 +87,5 @@ done
 echo "Cleaning up the temporary directory..."
 rm -rf $temp_dir
 
-echo "Setup complete! Enjoy your new development environment."
+echo -e "\nSetup complete! Enjoy your new development environment."
 # TODO: Add Neovim setup script.
